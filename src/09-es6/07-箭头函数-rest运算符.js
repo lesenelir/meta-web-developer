6f815ba2e46f339c793f8adf2ae6088926c78e51
lesenelir <miaomiaobabyzy@gmail.com>
2022-06-28 18:36:18 +0800

    udpate

--- src/09-es6/07-箭头函数-rest运算符.js
+++ src/09-es6/07-箭头函数-rest运算符.js
@@ -5,6 +5,19 @@
  *    3. 显示绑定： call apply bind
  *    4. new方式
  *
+ *  箭头函数：
+ *    1. 箭头函数的this是由外层函数作用域决定的
+ *    2. 箭头函数不能当做构造函数
+ *    3. 箭头函数没有arguments对象，用rest运算符替代
+ *    4. yield 命令在 generator函数中不能生效
+ *
+ *  适合箭头函数使用场景：
+ *    1. 简单的表达函数表达式，得出唯一的return计算值，函数内部没有this
+ *       没有递归、没有事件绑定、没有解绑定，用重构 => 的方式
+ *    2. 内层的函数表达式需要调用this
+ *    3. let args = Array.prototype.slice.call(arguments) 这种情况可以用箭头函数
+ *
+ *
  */
 
 // 参数一个省略括号，一条语句可以省略return
@@ -71,4 +84,45 @@ function sortNum() {
 let sortNum2 = (...args) => { // 剩余参数
   return args.sort((a, b) => a - b)
 }
+
+
+// 箭头函数的this
+function f6() {
+  console.log(this) // obj1
+  return () => {
+    console.log(this.a)
+  }
+}
+let obj1 = {a: 2}
+let obj2 = {a: 3}
+let bar = f6.call(obj1) // f6挂载到obj1执行，此时this为obj1，返回的箭头函数赋值给变量bar
+bar.call(obj2) // 2 把箭头bar挂载到obj2执行，但由于是箭头函数，this还是obj1
+
+
+const person = {
+  eat() {
+    console.log(this)
+  },
+  drink: () => {
+    console.log(this)
+  }
+}
+
+person.eat() // person
+person.drink() // {}
+
+
+// 箭头函数arguments不存在
+function f7() {
+  setTimeout(() => {
+    console.log(arguments) // 此处的arguments是外层函数的arguments
+  }, 1000)
+}
+f7(1,2,3,4)
+
+
+
+
+
+
 
